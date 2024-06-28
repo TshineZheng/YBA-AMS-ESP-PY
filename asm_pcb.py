a82bb018047fc0b4cd32219b8e0cbf5f0a12db42
Tshine <gtshine@gmail.com>
2024-06-28 18:15:57 +0800

    单缓冲

--- asm_pcb.py
+++ asm_pcb.py
@@ -35,18 +35,20 @@ ch_io = [
          Pin(M4_DOWN, Pin.OUT, Pin.PULL_DOWN)],
 ]
 
-sw_io = [
-    Pin(SW1, Pin.IN, Pin.PULL_DOWN), Pin(SW2, Pin.IN, Pin.PULL_DOWN), 
-    Pin(SW3, Pin.IN, Pin.PULL_DOWN), Pin(SW4, Pin.IN, Pin.PULL_DOWN)
-]
-
 led_power_io = Pin(LED1, Pin.OUT, Pin.PULL_DOWN)
 led_connect_io = Pin(LED2, Pin.OUT, Pin.PULL_DOWN)
 
-channel_status = [ACTION.STOP, ACTION.STOP, ACTION.STOP, ACTION.STOP]
-switch_status = [0, 0, 0, 0]
+cur_ch: int = 0
+cur_ch_action: int = ACTION.STOP
 
-def motor_control(id: int, action: int, status_save=True):
+latest_state = None
+
+buffer_1 = Pin(SW1, Pin.IN, Pin.PULL_DOWN)
+buffer_2 = Pin(SW2, Pin.IN, Pin.PULL_DOWN)
+broken_detect = Pin(SW3, Pin.IN, Pin.PULL_DOWN)
+
+
+def _motor_control(id: int, action: int):
     if action == ACTION.PULL:
         ch_io[id][0].value(1)
         ch_io[id][1].value(0)
@@ -57,8 +59,26 @@ def motor_control(id: int, action: int, status_save=True):
         ch_io[id][0].value(0)
         ch_io[id][1].value(0)
 
-    if status_save:
-        channel_status[id] = action
+
+def _motor_set(id: int, action: int):
+    for i in range(4):
+        if i != id:
+            _motor_control(i, ACTION.STOP)
+        else:
+            _motor_control(i, action)
+
+
+def motor_triiger(id: int, action: int):
+    global cur_ch
+    global cur_ch_action
+    
+    if cur_ch == id and cur_ch_action == action:
+        return
+    
+    cur_ch = id
+    cur_ch_action = action
+    _motor_control(id, action)
+
 
 def gpio_init():
 
@@ -69,41 +89,58 @@ def gpio_init():
     led_power_io.value(1)
     led_connect_io.value(0)
 
+
 def self_check():
     for i in range(4):
         print(f"channel {i} testing")
-        motor_control(i, ACTION.PULL)
+        _motor_control(i, ACTION.PULL)
         time.sleep(0.5)
-        motor_control(i, ACTION.PUSH)
+        _motor_control(i, ACTION.PUSH)
         time.sleep(0.5)
-        motor_control(i, ACTION.STOP)
+        _motor_control(i, ACTION.STOP)
         time.sleep(0.5)
 
-def logic():
-    for i in range(4):
-        if channel_status[i] == ACTION.PULL:
-            # 回抽时，如果上微动触发则停止回抽
-            if sw_io[i].value() == 1:
-                motor_control(i, ACTION.STOP, False)
-            else:
-                motor_control(i, ACTION.PULL, False)
+def get_broken_state():
+    return broken_detect.value()
+
+def logic(onbrokenSwitch):
+    global cur_ch
+    global cur_ch_action
+    global latest_state
+
+    currentState = broken_detect.value()
+    if currentState != latest_state:
+            latest_state = currentState
+            if onbrokenSwitch:
+                onbrokenSwitch(latest_state)
+
+    if cur_ch_action == ACTION.PUSH:
+        if buffer_2.value() == 1:
+            _motor_set(cur_ch, ACTION.STOP)
         else:
-            if sw_io[i].value() == 1:
-                switch_status[i] = 5
-                motor_control(i, ACTION.PUSH, False)
-            elif switch_status[i] > 0:
-                if switch_status[i] == 1:
-                    motor_control(i, channel_status[i])
-                switch_status[i] -= 1
+            _motor_set(cur_ch, ACTION.PUSH)
+    elif cur_ch_action == ACTION.PULL:
+        if buffer_1.value() == 1:
+            _motor_set(cur_ch, ACTION.STOP)
+        else:
+            _motor_set(cur_ch, ACTION.PULL)
+    else:
+        if buffer_1.value() == 1:
+            _motor_set(cur_ch, ACTION.PUSH)
+        elif buffer_2.value() == 1:
+            _motor_set(cur_ch, ACTION.PULL)
+        else:
+            _motor_set(cur_ch, ACTION.STOP)
+
 
 if __name__ == '__main__':
     gpio_init()
-    self_check()
+    # self_check()
+    motor_triiger(3, ACTION.PUSH)
 
     try:
         while True:
             logic()
-            time.sleep(0.1)
+            time.sleep(0.05)
     except KeyboardInterrupt:
         gpio_init()
- 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import _thread
 import gc
+import json
 import time
 
 import esp  # type: ignore
@@ -11,6 +12,10 @@ from mpy_common.simple_log import log_boot
 from mpy_common.time_util import sync_ntp
 from socket_server import SocketServer
 
+msg_type_gc = 0
+msg_type_sys = 1
+msg_type_broken_detect = 2
+
 
 def on_socket_recv(client, data):
     data_len = len(data)
@@ -19,7 +24,7 @@ def on_socket_recv(client, data):
     if data_len == 8 and data[0:6] == b'\x2f\x2f\xff\xfe\x01\x02':  # YBA 原来的指令
         ch = int(data[-2])
         fx = int(data[-1])
-        ams.motor_control(ch, fx)
+        ams.motor_triiger(ch, fx)
 
     elif data_len >= 5 and data[0:4] == b'\x2f\x2f\xff\xfe':
         cmd = data[4:5]
@@ -29,23 +34,29 @@ def on_socket_recv(client, data):
                 fx1 = int.from_bytes(data[7:8], 'big')
                 fx2 = int.from_bytes(data[8:9], 'big')
                 fx3 = int.from_bytes(data[9:10], 'big')
-                ams.motor_control(0, fx0)
-                ams.motor_control(1, fx1)
-                ams.motor_control(2, fx2)
-                ams.motor_control(3, fx3)
+                if fx0 != 0:
+                    ams.motor_triiger(0, fx0)
+                if fx1 != 0:
+                    ams.motor_triiger(1, fx1)
+                if fx2 != 0:
+                    ams.motor_triiger(2, fx2)
+                if fx3 != 0:
+                    ams.motor_triiger(3, fx3)
         elif b'\xff' == cmd:    # gc
             free = gc.mem_free()
             alloc = gc.mem_alloc()
             gc.collect()
             gc_free = gc.mem_free()
             gc_alloc = gc.mem_alloc()
-            socket_server.send(
-                client, f'esp32c3 memory alloc:{alloc}, free:{free}, gc alloc:{gc_alloc}, gc free:{gc_free}')
+            socket_server.send(client, generate_msg(
+                msg_type_gc, f'esp32c3 memory alloc:{alloc}, free:{free}, gc alloc:{gc_alloc}, gc free:{gc_free}'))
         elif b'\xfe' == cmd:    # 获取系统状态信息
             gc_free = gc.mem_free()
             gc_alloc = gc.mem_alloc()
-            socket_server.send(
-                client, f'esp32c3 memory alloc:{gc_alloc}, free:{gc_free}')
+            socket_server.send(client, generate_msg(
+                msg_type_sys, f'esp32c3 memory alloc:{gc_alloc}, free:{gc_free}'))
+        elif b'\xfd' == cmd:    # 获取断料状态
+            sendBrokenState(ams.broken_detect.value())
 
     return True
 
@@ -53,11 +64,15 @@ def on_socket_recv(client, data):
 def on_client_connect(client, address):
     ams.led_connect_io.value(1)
 
+
 def on_client_disconnect(client, address):
     # TODO:此处最好判断下有几个客户端，如果还有客户端就不要关闭，虽然目前按逻辑只有一个
     ams.led_connect_io.value(0)
 
+
 power_led_shake = True
+
+
 def on_wifi_state(connected):
     global power_led_shake
     if connected:
@@ -65,6 +80,18 @@ def on_wifi_state(connected):
     else:
         ams.led_power_io.value(0 if power_led_shake else 1)
         power_led_shake = not power_led_shake
+
+
+def sendBrokenState(state: int):
+    socket_server.send_all(generate_msg(msg_type_broken_detect, str(state)))
+
+
+def generate_msg(type: int, data: str):
+    return json.dumps({
+        'type': type,
+        'data': data
+    })
+
 
 esp.osdebug(0)  # 禁用调试
 # esp.sleep_type(esp.SLEEP_NONE)  # 禁用休眠
@@ -79,13 +106,14 @@ print("可用内存：", gc.mem_free())  # 查看当前可用的内存
 
 # ams.self_check()
 
-thread_id = _thread.start_new_thread(wifi_connect.wifi_check, (on_wifi_state, ))  # 启动Wifi检测线程
+thread_id = _thread.start_new_thread(
+    wifi_connect.wifi_check, (on_wifi_state, ))  # 启动Wifi检测线程
 
 wifi_connect.wifi_init()  # 先连接网络
 sync_ntp(sync_interval=0.1)  # 同步时间
 log_boot()  # 记录启动
 
-webrepl.start(password='123456') # 启动 webrepl
+webrepl.start(password='123456')  # 启动 webrepl
 
 socket_server = SocketServer('0.0.0.0', 3333, on_socket_recv,
                              need_send=True,
@@ -100,7 +128,7 @@ print(f"执行垃圾回收后的可用内存：{gc.mem_free()}")
 
 try:
     while True:
-        ams.logic()
+        ams.logic(sendBrokenState)
         time.sleep(0.1)
 except:
     ams.gpio_init()
